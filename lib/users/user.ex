de5561f60b8456b79b89934ebe2477b8e22cdded
RafaelManfrim <rafaelmanfrim2004@gmail.com>
2022-07-18 23:53:43 -0300

    função build struct User

--- lib/users/user.ex
+++ lib/users/user.ex
@@ -1,3 +1,17 @@
 defmodule Exlivery.Users.User do
-  defstruct [:name, :email, :cpf, :age]
+  @keys [:name, :email, :cpf, :age]
+  @enforce_keys @keys
+  defstruct @keys
+
+  def build(name, email, cpf, age) when age >= 18 and is_bitstring(cpf) do
+    {:ok,
+     %__MODULE__{
+       name: name,
+       email: email,
+       cpf: cpf,
+       age: age
+     }}
+  end
+
+  def build(__name, __email, __cpf, __age), do: {:error, "Invalid parameters"}
 end
