3f61557e5677267d70c4695aeb3ff0452de9b46f
RafaelManfrim <rafaelmanfrim2004@gmail.com>
2022-07-19 16:46:59 -0300

    finalizando build de Order

--- lib/orders/order.ex
+++ lib/orders/order.ex
@@ -1,16 +1,29 @@
 defmodule Exlivery.Orders.Order do
+  alias Exlivery.Orders.Item
+  alias Exlivery.Users.User
+
   @keys [:user_cpf, :delivery_address, :items, :total_price]
   @enforce_keys @keys
 
   defstruct @keys
 
-  def build do
+  def build(%User{cpf: cpf, address: address}, [%Item{} | _items] = items) do
     {:ok,
      %__MODULE__{
-       user_cpf: nil,
-       delivery_address: nil,
-       items: nil,
-       total_price: nil
+       user_cpf: cpf,
+       delivery_address: address,
+       items: items,
+       total_price: calculate_total_price(items)
      }}
+  end
+
+  def build(_user, _items), do: {:error, "Invalid parameters"}
+
+  defp calculate_total_price(items) do
+    Enum.reduce(items, Decimal.new("0.00"), fn item, acc -> sum_prices(item, acc) end)
+  end
+
+  defp sum_prices(%Item{unity_price: price, quantity: quantity}, acc) do
+    price |> Decimal.mult(quantity) |> Decimal.add(acc)
   end
 end
--- lib/users/user.ex
+++ lib/users/user.ex
@@ -1,17 +1,18 @@
 defmodule Exlivery.Users.User do
-  @keys [:name, :email, :cpf, :age]
+  @keys [:name, :email, :address, :cpf, :age]
   @enforce_keys @keys
   defstruct @keys
 
-  def build(name, email, cpf, age) when age >= 18 and is_bitstring(cpf) do
+  def build(name, email, address, cpf, age) when age >= 18 and is_bitstring(cpf) do
     {:ok,
      %__MODULE__{
        name: name,
        email: email,
+       address: address,
        cpf: cpf,
        age: age
      }}
   end
 
-  def build(__name, __email, __cpf, __age), do: {:error, "Invalid parameters"}
+  def build(_name, _email, _address, _cpf, _age), do: {:error, "Invalid parameters"}
 end
