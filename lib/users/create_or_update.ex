9ebee5f618312a782d6dbb06e83f3a2e1da80734
RafaelManfrim <rafaelmanfrim2004@gmail.com>
2022-07-22 14:05:50 -0300

    Ajustando erro no create_or_update de user

--- lib/users/create_or_update.ex
+++ lib/users/create_or_update.ex
@@ -2,8 +2,8 @@ defmodule Exlivery.Users.CreateOrUpdate do
   alias Exlivery.Users.Agent, as: UserAgent
   alias Exlivery.Users.User
 
-  def call(%{name: name, address: address, email: email, cpf: cpf, age: age}) do
-    address |> User.build(name, email, cpf, age) |> save_user()
+  def call(%{name: name, email: email, address: address, cpf: cpf, age: age}) do
+    User.build(name, email, address, cpf, age) |> save_user()
   end
 
   defp save_user({:ok, %User{} = user}), do: UserAgent.save(user)
