a42f7136fba5fed6ca198983e6c79a71505dc631
RafaelManfrim <rafaelmanfrim2004@gmail.com>
2022-07-22 22:59:29 -0300

    CreateOrUpdateOrderTest

---
+++ test/orders/create_or_update_test.exs
@@ -0,0 +1,65 @@
+defmodule Exlivery.Orders.CreateOrUpdateTest do
+  use ExUnit.Case
+  import Exlivery.Factory
+  alias Exlivery.Orders.CreateOrUpdate
+  alias Exlivery.Users.Agent, as: UserAgent
+
+  describe "call/1" do
+    setup do
+      Exlivery.start_agents()
+      cpf = "12345"
+      user = build(:user, cpf: cpf)
+
+      UserAgent.save(user)
+
+      item1 = build(:item)
+      item2 = build(:item, description: "Pizza de 4 Queijos")
+
+      {:ok, user_cpf: cpf, item1: item1, item2: item2}
+    end
+
+    test "when all params are valid, saves the order", %{
+      user_cpf: cpf,
+      item1: item1,
+      item2: item2
+    } do
+      params = %{user_cpf: cpf, items: [item1, item2]}
+
+      response = CreateOrUpdate.call(params)
+
+      assert {:ok, _uuid} = response
+    end
+
+    test "when there is no user with given cpf, returns an error", %{item1: item1, item2: item2} do
+      params = %{user_cpf: "invalid_cpf", items: [item1, item2]}
+
+      response = CreateOrUpdate.call(params)
+
+      assert {:error, "User not found"} = response
+    end
+
+    test "when there are invalid items, returns an error", %{
+      user_cpf: cpf,
+      item1: item1,
+      item2: item2
+    } do
+      params = %{user_cpf: cpf, items: [item1, %{item2 | quantity: 0}]}
+
+      response = CreateOrUpdate.call(params)
+
+      expected_response = {:error, "Invalid items"}
+
+      assert response == expected_response
+    end
+
+    test "when there are no items, returns an error", %{user_cpf: cpf} do
+      params = %{user_cpf: cpf, items: []}
+
+      response = CreateOrUpdate.call(params)
+
+      expected_response = {:error, "Invalid parameters"}
+
+      assert response == expected_response
+    end
+  end
+end
