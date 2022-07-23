4f1ae3b2ae46531992d8e909ca1354e9d8644a55
RafaelManfrim <rafaelmanfrim2004@gmail.com>
2022-07-22 21:04:59 -0300

    teste UserAgent

--- test/exlivery_test.exs
+++ test/exlivery_test.exs
@@ -1,8 +1,4 @@
 defmodule ExliveryTest do
   use ExUnit.Case
   doctest Exlivery
-
-  test "greets the world" do
-    assert Exlivery.hello() == :world
-  end
 end
---
+++ test/users/agent_test.exs
@@ -0,0 +1,46 @@
+defmodule Exlivery.Users.AgentTest do
+  use ExUnit.Case
+  alias Exlivery.Users.Agent, as: UserAgent
+  alias Exlivery.Users.User
+  import Exlivery.Factory
+
+  describe "save/1" do
+    test "saves the user" do
+      user = build(:user)
+      UserAgent.start_link(%{})
+      assert UserAgent.save(user) == :ok
+    end
+  end
+
+  describe "get/1" do
+    setup do
+      UserAgent.start_link(%{})
+      :ok
+    end
+
+    test "when the user is found, returns the user" do
+      user = build(:user, cpf: "12345")
+      UserAgent.save(user)
+
+      response = UserAgent.get("12345")
+
+      expected_response =
+        {:ok,
+         %User{
+           address: "Rua Carvalho Leme",
+           age: 18,
+           cpf: "12345",
+           email: "rafael@gmail.com",
+           name: "Rafael"
+         }}
+
+      assert response == expected_response
+    end
+
+    test "when the user is not found, returns an error" do
+      response = UserAgent.get("12345")
+      expected_response = {:error, "User not found"}
+      assert response == expected_response
+    end
+  end
+end
